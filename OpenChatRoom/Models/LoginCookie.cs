98b81c2ddc33a276c516bb6c2b377c8a09193ecd
Pierouge <louis.github@famillebauer.org>
2025-05-22 16:49:36 +0200

    Small additions to the Login/register system

--- OpenChatRoom/Models/LoginCookie.cs
+++ OpenChatRoom/Models/LoginCookie.cs
@@ -1,13 +1,13 @@
 using System;
 using System.Security.Cryptography;
 using System.Text;
+using System.Threading.Tasks;
 
 public class LoginCookie(IConfiguration config, string serverIp)
 {
     private readonly IConfiguration configuration = config;
     private readonly string Server = serverIp;
     private string Username = string.Empty;
-    private string visibleName = string.Empty;
     private string Password = string.Empty;
     private string Salt = string.Empty;
 
@@ -56,8 +56,10 @@ public class LoginCookie(IConfiguration config, string serverIp)
         string? port = configuration["PORT"];
         string ServerUrl = string.Concat(Server, ":", port, "/check");
         if (!(ServerUrl.Contains("http://")||ServerUrl.Contains("https://"))) ServerUrl = string.Concat("http://", ServerUrl);
-        HttpClient httpClient = new();
-        httpClient.Timeout = TimeSpan.FromSeconds(5);
+        HttpClient httpClient = new()
+        {
+            Timeout = TimeSpan.FromSeconds(5)
+        };
         try
         {
             HttpResponseMessage response = httpClient.GetAsync(ServerUrl).Result;
@@ -76,23 +78,59 @@ public class LoginCookie(IConfiguration config, string serverIp)
     public bool login()
     {
         string? port = configuration["PORT"];
+        string ServerUrl = string.Concat(Server, ":", port, "/user/", Username, "/", Password);
+        if (!(ServerUrl.Contains("http://")||ServerUrl.Contains("https://"))) ServerUrl = string.Concat("http://", ServerUrl);
         return false;
     }
 
     // To register, setting the visible name aswell
-    public bool register(string visibleNm)
+    public bool register(string visibleName)
     {
         string? port = configuration["PORT"];
-        visibleName = visibleNm;
+        string ServerUrl = string.Concat(Server, ":", port, "/user");
+        if (!(ServerUrl.Contains("http://")||ServerUrl.Contains("https://"))) ServerUrl = string.Concat("http://", ServerUrl);
+
         return false;
     }
 
     // To update the different fields of the cookie
-    public bool updateInfo(string usernm, string pass)
+    public async Task<bool> updateInfo(string usernm, string pass, bool newSalt)
     {
         Username = usernm;
 
-        return false;
+        byte[] saltBytes = new byte[16];
+        // Generate the new salt
+        if (newSalt)
+        {
+            saltBytes = new byte[16];
+            RandomNumberGenerator.Fill(saltBytes);
+            Salt = Convert.ToBase64String(saltBytes);
+        }
+        // Fetch the previous salt
+        else
+        {
+            string? port = configuration["PORT"];
+            string ServerUrl = string.Concat(Server, ":", port, "/user/", Username);
+            if (!(ServerUrl.Contains("http://") || ServerUrl.Contains("https://"))) ServerUrl = string.Concat("http://", ServerUrl);
+            HttpClient httpClient = new()
+            {
+                Timeout = TimeSpan.FromSeconds(5)
+            };
+            HttpResponseMessage response = await httpClient.GetAsync(ServerUrl);
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                Salt = await response.Content.ReadAsStringAsync();
+                saltBytes = Convert.FromBase64String(Salt);
+            }
+            else return false;
+        }
+
+        // Hash the password 10k times
+        using var pbkdf2 = new Rfc2898DeriveBytes(pass, saltBytes, 10_000, HashAlgorithmName.SHA256);
+        byte[] hashBytes = pbkdf2.GetBytes(32);
+
+        Password = Convert.ToBase64String(hashBytes);
+        return true;
     }
 
     // To get a Cookie from a cookie string
